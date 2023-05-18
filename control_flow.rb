72e3aefd21cb2061c9b5bed61127bbe82bfd15cb
GeoReece000 <geofreytegeret@gmail.com>
2023-05-18 20:05:00 +0300

    Started

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,16 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if 
+  
 end
 
 def fizzbuzz(num)
